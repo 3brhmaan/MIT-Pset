0984ca1e959fb4cb973f7b8a7c7d1862f7cf6960
3brhman <77367068+3brhmn@users.noreply.github.com>
2021-08-22 07:22:40 +0200

    Add files via upload

---
+++ ps1/ps1b.py
@@ -0,0 +1,19 @@
+annual_salary = float(input("Enter your annual salary : "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal : "))
+total_cost = float(input("Enter the cost of your dream home : "))
+semi_annual_raise = float(input("Enter the semiannual raise, as a decimal : "))
+
+salary_saved_evry_month = (annual_salary / 12)*portion_saved
+portion_down_payment = .25 * total_cost
+mont_rate = .04 / 12
+saved = 0.0
+saved = salary_saved_evry_month + saved*mont_rate
+month = 1
+
+while saved < portion_down_payment :
+    saved += salary_saved_evry_month  + saved*mont_rate
+    month+=1
+    if month % 6==0:
+        salary_saved_evry_month=salary_saved_evry_month*(1+semi_annual_raise)
+
+print("Number of months :",month)
